ade0da45b76d2e65911b19cb9b141c34fc6efa7d
lizaaaua <lizanecheporenko@gmail.com>
2026-06-03 00:49:16 +0300

    Update recipes.py

--- recipes.py
+++ recipes.py
@@ -1,6 +1,39 @@
 class Ingredient:
-    pass
 
+    def __init__(self, name: str, quantity: float, unit: str):
+        self._name = name
+        self._quantity = None
+        self._unit = unit
+        self.quantity = quantity
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def quantity(self) -> float:
+        return self._quantity
+
+    @quantity.setter
+    def quantity(self, value) -> None:
+        value = float(value)
+
+        if value <= 0:
+            raise ValueError
+
+        self._quantity = value
+
+    @property
+    def unit(self) -> str:
+        return self._unit
+    def __str__(self) -> str:
+        return f"{self.name}: {self.quantity} {self.unit}"
+    def __repr__(self) -> str:
+        return f"Ingredient('{self.name}', {self.quantity}, '{self.unit}')"
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, Ingredient):
+            return False
+        return self.name == other.name and self.unit == other.unit
 
 class Recipe:
     pass
